de568e4aea57d1e616a774b412a6cc5b43ec58e7
Cameron <malachor@bigpond.net.au>
2021-09-08 21:44:22 +1000

    Added Server.js and routes/api.js

--- assignment/server/server.js
+++ assignment/server/server.js
@@ -1,3 +1,18 @@
 var express = require('express');
+var path = require('path');
 var app = express();
-var http = require('http').Server(app);
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+const PORT = 3000;
+
+require('./routes/api.js') (app);
+app.use(express.static(path.join(__dirname, '../dist/assignment')));
+
+app.listen(PORT, () =>{
+    console.log('Server started on port: '+ PORT);
+});
